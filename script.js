3539c86310736aff1f78cbbb5d49a4f589153b8b
Andrew Crow <andrew@crow.me>
2020-09-18 10:25:51 -0700

    added multiple functions for weather, ip location, date conversion

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Converts city name into latitude and longitude and inputs that into currentWeather
 function findLatLon(city) {
     var query1URL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=05b151abf8878f4a65f1f748137f62da";
     
@@ -13,6 +15,7 @@ function findLatLon(city) {
 
 }
 
+// Grabs latitude and longitude and responds with current weather and 7 day forecast
 function currentWeather(lat,lon) {
 var query2URL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=692496d9b9647012326807b41694aa6b";
     $.ajax({
@@ -23,22 +26,30 @@ var query2URL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "
     });
 }
 
-var d = new Date(1600414810)
+// Converts Unix TimeStamp to MM/DD/YYYY
+function convertDate(unixTimeStamp){
+    let date = new Date(unixTimeStamp*1000);
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    return month + "/" + day + "/" + year
+}
 
-console.log(d)
 
-// function forecastWeather(city) {
-//     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=05b151abf8878f4a65f1f748137f62da";
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response)
-        
-//     });
-// }
 
-findLatLon("Seattle");
-// forecastWeather("Seattle");
+// Grabs users IP and gives Latitude and Longitude
+function currentLocation () {
+    $.ajax({
+        url: "http://ip-api.com/json/",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.lat);
+        console.log(response.lon);
+        console.log(response.city);
+        currentWeather(response.lat,response.lon)
+    });
+}
 
-// http://api.openweathermap.org/data/2.5/forecast/daily?q=seattle&cnt=5&appid=05b151abf8878f4a65f1f748137f62da
+console.log(convertDate(1600414810));
+currentLocation (); 
+// findLatLon("San Francisco");
